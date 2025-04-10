42913ad5042c6f76e68c69adb0fc3255fbe7ae1a
xdydy <dydy@i2ec.top>
2025-04-10 09:29:46 +0800

    remove driver

--- demo/customfunc/driver.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-os.environ["FAASIT_PROVIDER"]="local-once"
-from index import handler
-start_time = time.time()
-output = handler({})
-end_time = time.time()
-print(output)
-print(f"Execution time: {end_time - start_time} seconds")
