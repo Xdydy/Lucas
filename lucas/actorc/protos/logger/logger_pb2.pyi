80dd64396722dfce07d1f109304cd8c825ffc76f
kekwy <keekkewy@qq.com>
2025-12-23 14:05:22 +0800

    fix

--- lucas/actorc/protos/logger/logger_pb2.pyi
+++ lucas/actorc/protos/logger/logger_pb2.pyi
@@ -7,25 +7,23 @@ from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class LogStreamMessage(_message.Message):
-    __slots__ = ("component_id", "entry", "control")
-    COMPONENT_ID_FIELD_NUMBER: _ClassVar[int]
+    __slots__ = ("application_id", "entry")
+    APPLICATION_ID_FIELD_NUMBER: _ClassVar[int]
     ENTRY_FIELD_NUMBER: _ClassVar[int]
-    CONTROL_FIELD_NUMBER: _ClassVar[int]
-    component_id: str
+    application_id: str
     entry: _logger_pb2.LogEntry
-    control: _logger_pb2.StreamControl
-    def __init__(self, component_id: _Optional[str] = ..., entry: _Optional[_Union[_logger_pb2.LogEntry, _Mapping]] = ..., control: _Optional[_Union[_logger_pb2.StreamControl, _Mapping]] = ...) -> None: ...
+    def __init__(self, application_id: _Optional[str] = ..., entry: _Optional[_Union[_logger_pb2.LogEntry, _Mapping]] = ...) -> None: ...
 
 class LogStreamResponse(_message.Message):
-    __slots__ = ("component_id", "success", "error", "processed_count", "message")
-    COMPONENT_ID_FIELD_NUMBER: _ClassVar[int]
+    __slots__ = ("application_id", "success", "error", "processed_count", "message")
+    APPLICATION_ID_FIELD_NUMBER: _ClassVar[int]
     SUCCESS_FIELD_NUMBER: _ClassVar[int]
     ERROR_FIELD_NUMBER: _ClassVar[int]
     PROCESSED_COUNT_FIELD_NUMBER: _ClassVar[int]
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
-    component_id: str
+    application_id: str
     success: bool
     error: str
     processed_count: int
     message: str
-    def __init__(self, component_id: _Optional[str] = ..., success: bool = ..., error: _Optional[str] = ..., processed_count: _Optional[int] = ..., message: _Optional[str] = ...) -> None: ...
+    def __init__(self, application_id: _Optional[str] = ..., success: bool = ..., error: _Optional[str] = ..., processed_count: _Optional[int] = ..., message: _Optional[str] = ...) -> None: ...
